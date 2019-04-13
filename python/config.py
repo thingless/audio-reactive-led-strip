20503452cd060d448cc2fd25102845158e993696
Richard Klafter <richard@zinc.com>
2019-04-13 16:36:49 -0600

    config

--- python/config.py
+++ python/config.py
@@ -56,7 +56,7 @@ USE_GUI = True
 DISPLAY_FPS = True
 """Whether to display the FPS when running (can reduce performance)"""
 
-N_PIXELS = 15
+N_PIXELS = 4
 """Number of pixels in the LED strip (must match ESP8266 firmware)"""
 
 GAMMA_TABLE_PATH = os.path.join(os.path.dirname(__file__), 'gamma_table.npy')
